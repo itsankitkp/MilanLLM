b36e00138196ffa8ae55fc56d1ab1f3f4e988fb9
Ankit Kumar Pandey <itsankitkp@gmail.com>
2024-05-09 22:07:40 +0530

    Add data store

---
+++ src/milanllm/data_store/fetch.py
@@ -0,0 +1,80 @@
+import mwparserfromhell
+import requests
+from tqdm import tqdm
+
+def get_wikitext(topic: str)->str:
+    data = requests.get(
+        url='https://en.wikipedia.org/w/api.php',
+        params={
+            "action":"parse",
+            "page":topic,
+            "prop":"wikitext",
+            "formatversion":2,
+            "format":"json"
+        }
+    )
+    data_json = data.json()
+    wikitext = data_json['parse']['wikitext']
+    final_text =mwparserfromhell.parse(wikitext).strip_code()
+    return final_text 
+
+def get_categories(title: str)-> list[str]:
+    data = requests.get(
+        url='https://en.wikipedia.org/w/api.php',
+        params={
+            'action': 'query',
+            'format':'json',
+            'titles': title,
+            'prop': 'categories'
+        }
+    )
+
+    data_json = data.json()
+    pages = data_json['query']['pages']
+    categories=[]
+    
+    for _, v in pages.items():
+        categories=v['categories']
+        break
+
+    final_cat=[]
+
+    for item in categories:
+        final_cat.append(item['title'])
+
+    return final_cat
+
+def get_search(topic: str)->list[str]:
+    data = requests.get(
+        url='https://en.wikipedia.org/w/api.php',
+        params={
+            'action': 'query',
+            'format':'json',
+            'srsearch': topic,
+            'utf8': None,
+            'list':'search',
+            'srlimit': 500
+        }
+    )
+    data_json = data.json()
+    
+    search = data_json['query']['search']
+    titles= []
+    for item in search:
+        titles.append(item['title'])
+
+    return titles
+    
+def get_corpus(topic)->str:
+    topics = get_search(topic)
+
+    corpus = []
+    for t in tqdm(topics):
+        txt = get_wikitext(t)
+        corpus.append(txt)
+
+    return corpus
+
+def write_to_disk(corpus, fname='corpus.txt'):
+    with open(fname,'w+') as f:
+        f.write(corpus)
